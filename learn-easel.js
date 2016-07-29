d1468f3c65614c8b1213f2eaa7e9796a68ee5b00
zhao tianyu <zhaotianyu@drecom.co.jp>
2016-07-29 13:32:07 +0900

    interaction部分追加

--- learn-easel.js
+++ learn-easel.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+  createShapeStage();
+  createInteractiveShapeStage();
+};
+
+function createShapeStage() {
   var shapeStage = new createjs.Stage('shapeCanvas');
 
   function drawRectangle() {
@@ -34,4 +39,31 @@ window.onload = function() {
   }
 
   drawShapes();
-};
+}
+
+function createInteractiveShapeStage() {
+  var interactiveShapeStage = new createjs.Stage('interactCanvas');
+
+  function drawShapes() {
+    for (var i = 0; i < 15; i++) {
+      var theShape = new createjs.Shape();
+      theShape.graphics.setStrokeStyle(1).beginStroke(createjs.Graphics.getRGB(0, 0, 0)).beginFill(createjs.Graphics.getRGB(255,255,51)).drawCircle(0, 0, 20).moveTo(-20, 0).lineTo
+  (20,0);
+      theShape.x = Math.floor(Math.random() * 600);
+      theShape.y = Math.floor(Math.random() * 300);
+      theShape.scaleX = Math.floor(Math.random() * 2) + 1;
+      theShape.scaleY = Math.floor(Math.random() * 2) + 1;
+      theShape.alpha = Math.random() * 1;
+      theShape.rotation = Math.floor(Math.random() * 360);
+      theShape.on("click", handleClick, null, false);            
+      interactiveShapeStage.addChild(theShape);
+    }
+    interactiveShapeStage.update();
+  }
+
+  function handleClick() {
+    alert('clicked!');
+  }
+
+  drawShapes();
+}
